c503fc9c88d53543e58b761300bfd4fd9e0e4580
Manaraceko_PM <manaraceko.manoppo@gmail.com>
2023-07-02 10:45:16 +0700

    initial commit

---
+++ src/pages/HelpPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore atque sit ratione totam odit inventore, aut vel cupiditate. Provident culpa dicta quod excepturi laboriosam nulla. Aliquid ex totam nisi ut!</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore atque sit ratione totam odit inventore, aut vel cupiditate. Provident culpa dicta quod excepturi laboriosam nulla. Aliquid ex totam nisi ut!</p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+})
+</script>
